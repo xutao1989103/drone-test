076af9c55ef8e8dab0c1b0adc93ba61a827bd6ac
xt111024 <xt111024@alibaba-inc.com>
2017-07-13 14:29:24 +0800

    remove jdk7

--- src/main/java/com/aone/mq/controller/DroneTestController.java
+++ src/main/java/com/aone/mq/controller/DroneTestController.java
@@ -1,6 +1,5 @@
 package com.aone.mq.controller;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
