d816f9a3459383888a135540d32454f83ad456ab
xt111024 <xt111024@alibaba-inc.com>
2017-07-13 10:16:43 +0800

    init git

---
+++ src/main/java/com/aone/mq/DroneDemoApplication.java
@@ -0,0 +1,12 @@
+package com.aone.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/aone/mq/controller/DroneTestController.java
@@ -0,0 +1,21 @@
+package com.aone.mq.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xutao
+ * @email xt111024@alibaba-inc.com
+ * @create 2017-07-13 上午9:47
+ */
+
+@RestController
+@RequestMapping("/drone")
+public class DroneTestController {
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public String test() {
+        return "ok";
+    }
+}
